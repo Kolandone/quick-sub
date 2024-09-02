ed07b0268fbf7b070512ddeedcf42cfe09dd82ab
KOLAND <sjamshid81@gmail.com>
2024-09-03 03:16:13 +0330

    Update sub-protocol.sh

--- sub-protocol.sh
+++ sub-protocol.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo -e "\e[1;32mSelect a VPN protocol for subscription:\e[0m"
 echo -e "\e[1;36m1. \e[1;33mall\e[0m"
 echo -e "\e[1;36m2. \e[1;33mVLESS\e[0m"
@@ -9,19 +8,24 @@ echo -e "\e[1;36m4. \e[1;33mSHADOWSOCKS\e[0m"
 echo -e "\e[1;36m5. \e[1;33mTROJAN\e[0m"
 echo -e "\e[1;36m6. \e[1;33mHysteria2\e[0m"
 
-
 echo -en "\e[1;32mEnter your choice: \e[0m"
-read -r choice
+read -r user_input
 
-
-case $choice in
-    1) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/mix";;
-    2) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/vless";;
-    3) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/vmess";;
-    4) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/ss";;
-    5) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/trojan";;
-    6) subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/hy2";;
-    *) echo -e "\e[1;31mInvalid choice\e[0m"; exit 1;;
-esac
+if [ "$user_input" -eq 1 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/mix"
+elif [ "$user_input" -eq 2 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/vless"
+elif [ "$user_input" -eq 3 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/vmess"
+elif [ "$user_input" -eq 4 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/ss"
+elif [ "$user_input" -eq 5 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/trojan"
+elif [ "$user_input" -eq 6 ]; then
+    subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/hy2"
+else
+    echo -e "\e[1;31mInvalid input.\e[0m"
+    exit 1
+fi
 
 echo -e "\e[1;32mYour VPN subscription link: \e[1;34m$subscription\e[0m"
