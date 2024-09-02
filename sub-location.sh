581ed3ff0b00dfb59fce30bf1790c4924054d2e4
KOLAND <sjamshid81@gmail.com>
2024-09-03 03:01:19 +0330

    Update sub-location.sh

--- sub-location.sh
+++ sub-location.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-clear
+
 echo -e "\e[1;35m*****************************************"
 echo -e "\e[1;35m*\e[0m \e[1;31mY\e[1;32mO\e[1;33mU\e[1;34mT\e[1;35mU\e[1;36mB\e[1;37mE\e[0m : \e[4;34mKOLANDONE\e[0m         \e[1;35m"
 echo -e "\e[1;35m*\e[0m \e[1;31mT\e[1;32mE\e[1;33mL\e[1;34mE\e[1;35mG\e[1;36mR\e[1;37mA\e[1;31mM\e[0m : \e[4;34mKOLANDJS\e[0m         \e[1;35m"
