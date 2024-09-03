9c49ac24c8152cb73c06290337f91d5949d06852
KOLAND <sjamshid81@gmail.com>
2024-09-03 03:31:33 +0330

    Update subprotocol.sh

--- subprotocol.sh
+++ subprotocol.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-# Define color variables
 green="\e[1;32m"
 cyan="\e[1;36m"
 yellow="\e[1;33m"
 red="\e[1;31m"
 blue="\e[1;34m"
 reset="\e[0m"
-
 echo -e "${green}Select a VPN protocol for subscription:${reset}"
 echo -e "${cyan}1. ${yellow}all${reset}"
 echo -e "${cyan}2. ${yellow}VLESS${reset}"
@@ -15,10 +12,8 @@ echo -e "${cyan}3. ${yellow}VMESS${reset}"
 echo -e "${cyan}4. ${yellow}SHADOWSOCKS${reset}"
 echo -e "${cyan}5. ${yellow}TROJAN${reset}"
 echo -e "${cyan}6. ${yellow}Hysteria2${reset}"
-
 echo -en "${green}Enter your choice: ${reset}"
 read -r user_input
-
 if [ "$user_input" -eq 1 ]; then
     subscription="https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/mix"
 elif [ "$user_input" -eq 2 ]; then
@@ -35,5 +30,4 @@ else
     echo -e "${red}Invalid input.${reset}"
     exit 1
 fi
-
 echo -e "${green}Your VPN subscription link: ${blue}$subscription${reset}"
